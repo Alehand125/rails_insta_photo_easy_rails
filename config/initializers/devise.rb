34968ba0d06c684e7ef17c87e37d002d182164d9
Alehand125 <Alehand125@gmail.com>
2018-05-16 17:44:54 +0600

    сделал автореформат кода, плюс прекомпиле

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'b4be5cf33351e85d725fe529ca9fec87afc5fbc6309399966d44e71ffdc1b5cebf965062a054a3fee25221c0821e6d8c9e72b5abd7f82710c366557825877f7d'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
--- test/controllers/photos_controller_test.rb
+++ test/controllers/photos_controller_test.rb
@@ -17,7 +17,7 @@ class PhotosControllerTest < ActionDispatch::IntegrationTest
 
   test "should create photo" do
     assert_difference('Photo.count') do
-      post photos_url, params: { photo: { description: @photo.description } }
+      post photos_url, params: {photo: {description: @photo.description}}
     end
 
     assert_redirected_to photo_url(Photo.last)
@@ -34,7 +34,7 @@ class PhotosControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update photo" do
-    patch photo_url(@photo), params: { photo: { description: @photo.description } }
+    patch photo_url(@photo), params: {photo: {description: @photo.description}}
     assert_redirected_to photo_url(@photo)
   end
 
