940c2e3cc2823b1423a45b2f7b67b710136a021d
Alehand125 <Alehand125@gmail.com>
2018-05-22 16:55:10 +0600

    добавил профиль пользователя, кликабельные ссылки на его профиль

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#landing'
   get 'pages/about'
+  # get '/profiles/id:' => 'profiles#show'
+  get '/profiles/:id' => 'profiles#show', as: "profile"
 end
